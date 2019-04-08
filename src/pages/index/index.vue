a8fb5e9e05685d76746d779c9ce32840b14d558f
v_chenchao <2020807070@qq.com>
2019-04-08 19:53:17 +0800

    feat: 滚动

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,75 +1,75 @@
 <template>
   <div class="wrapper">
-    <div class="top"></div>
-    <div class="bottom">
-      <div class="head"></div>
-      <div class="slogan">
-        Hello Word
-      </div>
-    </div>
+    <scroll-view
+      :style="{'height': '300px'}"
+      :scroll-y="true"
+      @scrolltolower="scrolltolower"
+      @scroll="scroll"
+    >
+      <div :style="{'height': '200px','background-color':'red'}">tyuiolpo</div>
+      <div class :style="{'height': '200px','background-color':'red'}">dsdsd</div>
+      <div class :style="{'height': '200px','background-color':'red'}">grytyju</div>
+    </scroll-view>
+
+    <!-- <div class="wrapper_wrapper" @scroll="scroll">
+      <div class="aaa" @click="scroll"></div>
+      <div class="aaa"></div>
+      <div class="aaa"></div>
+      <div class="aaa"></div>
+    </div>-->
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
+import card from "@/components/card";
 
 export default {
-  data () {
-    return {
-
-    }
+  data() {
+    return {};
   },
   components: {
     card
   },
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
+    scroll() {
+      console.log(123);
+    },
+    bindViewTap() {
+      const url = "../logs/main";
+      if (mpvuePlatform === "wx") {
+        mpvue.switchTab({ url });
       } else {
-        mpvue.navigateTo({ url })
+        mpvue.navigateTo({ url });
       }
     },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
+    clickHandle(ev) {
+      console.log("clickHandle:", ev);
       // throw {message: 'custom test'}
     }
   },
-  created () {
+  created() {
     // let app = getApp()
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
 /* @import '../../statics/css/global.scss'; */
 .wrapper {
   width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  .top {
+  height: 100%;
+  .wrapper_wrapper {
+    display: flex;
+    flex-direction: column;
     flex: 1;
-    background: #546471;
-  }
-  .bottom {
-    text-align: center;
-    height: 350rpx;
-    background: #ccc;
-    position: relative;
-    .head {
-      position: absolute;
-      left: 50%;
-      transform: translate3d(-50%, -50%, 0);
-      width: 150rpx;
-      height: 150rpx;
-      border-radius: 50%;
-      background: pink;
-    }
-    .slogan {
-      color: #fff;
-      margin-top: 80rpx;
+    overflow: scroll;
+    .aaa {
+      width: 100%;
+      height: 500rpx;
+      background-image: url(//mat1.gtimg.com/bbs/qqnewslite/images/mt-bg.jpg);
+      background-size: cover;
+      background-position: 50% 50%;
+      margin-bottom: 20rpx;
     }
   }
 }
